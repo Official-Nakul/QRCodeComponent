0b6b59c2f1584705e8a8ad947b97eb68c9142ca7
Official-Nakul <124872597+Official-Nakul@users.noreply.github.com>
2023-11-09 14:16:00 +0530

    Responsive QR Card template with felx-box and grid

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css";
+function App() {
+  return (
+    <>
+      <div className="cardBody">
+        <img src={"src/qrCode.jpg"} alt="" className="qrImg" />
+        <p className="heading">
+          Improve your front-end skills by building projects
+        </p>
+        <br />
+        <p className="txt">
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
